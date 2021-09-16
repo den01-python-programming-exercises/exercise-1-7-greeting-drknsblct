13eba915309569848ffbf15966d0092935b055b8
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-16 04:01:04 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,6 +1,6 @@
 def main():
     #write your code below this line
-    name = input('What is your name? ')
+    name = input('What is your name?')
     print(f'Hi {name}')
 
 if __name__ == '__main__':
